8f1e2f13820e3a8d93ac24486d0a4cda41f34c1f
info <info@rocqup.com>
2019-08-14 12:32:19 +0430

    Add retrofit required classes

---
+++ reliablemessaging/src/main/java/com/example/reliablemessaging/network/Url.java
@@ -0,0 +1,6 @@
+package com.example.reliablemessaging.network;
+
+public class Url {
+    public static final String BASE_URL = " https://challenge.ronash.co/";
+    public static final String SEND_MESSAGE = "reliable-messaging";
+}
