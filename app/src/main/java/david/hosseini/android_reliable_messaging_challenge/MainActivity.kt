deaeffd4e3383cafec0183f33d705206059053fc
Seyyed davud hosseiny <hosseinydavid@gmail.com>
2019-08-22 19:28:26 +0430

    Initial commit. add app and library module.

---
+++ app/src/main/java/david/hosseini/android_reliable_messaging_challenge/MainActivity.kt
@@ -0,0 +1,12 @@
+package david.hosseini.android_reliable_messaging_challenge
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
